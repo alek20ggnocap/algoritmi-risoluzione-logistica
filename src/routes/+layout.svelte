0a8bdd6b44b873a08bef1c4eb95490162d8500bb
alek20ggnocap <zanga.alessandro.studente@itispaleocapa.it>
2025-10-13 10:55:02 +0200

    inizio

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,6 @@
     let { children } = $props()
 </script>
 
-<div class="w-screen h-screen overflow-hidden bg-blue-400 relative">
+<div class="w-screen h-screen overflow-hiddenrelative">
     {@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
-<script>
-  
+<script lang="ts">
+    $: finsetraAttiva = "home";
+    const tabella: string[][] = [];
 </script>
 
-<div>
+<div class="h-full w-full overflow-x-hidden overflow-y-hidden">
+    <header class="h-[5%] bg-amber-600">
+        
+    </header>
+    <div class="h-[90%]">
 
+    </div>
+    <footer class="h-[5%] bg-amber-600">
+        <div class="flex flex-row items-center justify-center h-full">
+            <p>
+                by Bamba Amara, Biglioli Enea, Lamberto Luigi & Zanga Alessandro
+            </p>
+        </div>
+    </footer>
 </div>
