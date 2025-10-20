7dfc5f55ac1451b7c7cd25784c6cfca4ad315019
alek20ggnocap <zanga.alessandro.studente@itispaleocapa.it>
2025-10-20 09:22:22 +0200

    a

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     $: finsetraAttiva = "home";
-    const tabella: string[][] = [];
+    const tabella: number[][] = [];
 </script>
 
 <div class="h-full w-full overflow-x-hidden overflow-y-hidden">
@@ -8,7 +8,26 @@
         
     </header>
     <div class="h-[90%]">
+        <div>
 
+        </div>
+        
+        {#if finsetraAttiva === "home"}
+            <h1 class="text-4xl text-center mt-10">Benvenuto nella pagina principale!</h1>
+            
+        {/if}
+        {#if finsetraAttiva === "algoritmo nord ovest"}
+            <h1 class="text-4xl text-center mt-10">Informazioni su di noi</h1>
+            
+        {/if}
+        {#if finsetraAttiva === "algoritmo minimi costi"}
+            <h1 class="text-4xl text-center mt-10">Contattaci</h1>
+            
+        {/if}
+        {#if finsetraAttiva === "show"}
+            <h1 class="text-4xl text-center mt-10">FAQ</h1>
+            
+        {/if}
     </div>
     <footer class="h-[5%] bg-amber-600">
         <div class="flex flex-row items-center justify-center h-full">
